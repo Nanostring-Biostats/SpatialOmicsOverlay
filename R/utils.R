037ed6d42c59a3f017fbdf1744a344f911fd24ab
Maddy Griswold <mgriswold@nanostring.com>
2023-10-11 18:13:09 +0000

    allow for special characters in ROI names

--- R/utils.R
+++ R/utils.R
@@ -56,7 +56,7 @@ readLabWorksheet <- function(lw, slideName){
     
     lw <- read.table(lw, header = TRUE, sep = "\t", skip = startLine, 
                      fill = TRUE)
-    lw$ROILabel <- gsub("\"", "", gsub("=", "", lw$roi))
+    lw$ROILabel <- gsub("\\W", "", lw$roi)
     
     lw <- lw[lw$slide.name == slideName,]
     
